8cbec076ac32d11bae87fa3ae1bec223a05403d8
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-08-19 15:18:02 +0530

    Create display_resource.py

---
+++ display_resource.py
@@ -0,0 +1,7 @@
+# ============================================================
+# CELL 22: ADAPTIVE RESOURCE SELECTION RESULTS
+# ============================================================
+
+selection_df = pd.DataFrame(selection_records)
+
+display(selection_df)
